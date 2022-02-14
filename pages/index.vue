abdf65c1e743a5654df896b8bfde681473363fb9
--replace-all <fawzy.mohamed@live.com>
2022-02-14 08:57:02 +0300

    clean setup

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>The Login Page!</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
