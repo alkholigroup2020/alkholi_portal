9b96b9d79c5f2481ba82529fe0a567afb9ca139b
--replace-all <fawzy.mohamed@live.com>
2022-06-29 10:15:57 +0300

    dtr - finish the sub-projects listing from HR system

--- pages/administration/dtr-setup/sub-projects/_subProjects.vue
+++ pages/administration/dtr-setup/sub-projects/_subProjects.vue
@@ -40,7 +40,7 @@
           style="height: 50px"
           :to="
             localePath(
-              `/administration/dtr-setup/departments/${divisionCode}?branch=${branch}&divisionName=${divisionName}`
+              `/administration/dtr-setup/projects/${departmentCode}?branch=${branch}&division=${divisionCode}&divisionName=${divisionName}&departmentName=${departmentName}`
             )
           "
         >
@@ -81,6 +81,10 @@
           <v-icon color="primaryText">mdi-arrow-right-thin</v-icon>
         </div>
         <h6 class="text-body-1 primaryText--Text">{{ departmentName }}</h6>
+        <div class="px-2">
+          <v-icon color="primaryText">mdi-arrow-right-thin</v-icon>
+        </div>
+        <h6 class="text-body-1 primaryText--Text">{{ projectName }}</h6>
       </div>
     </v-toolbar>
 
@@ -88,53 +92,48 @@
       <v-row>
         <v-col class="pt-5 pb-0" cols="12">
           <div>
-            <p class="text-h5 mb-1">Projects</p>
+            <p class="text-h5 mb-1">Sub-Projects</p>
             <hr />
             <hr />
           </div>
         </v-col>
       </v-row>
-      <v-row v-if="allProjects.length > 0">
+      <v-row v-if="allSubProjects.length > 0">
         <v-col
-          v-for="(project, index) in allProjects"
+          v-for="(subProject, index) in allSubProjects"
           :key="index"
           cols="6"
           md="3"
           class="py-5"
         >
-          <v-card
-            elevation="1"
-            color="whiteColor"
-            outlined
-            nuxt
-            :to="
-              localePath(
-                `/administration/dtr-setup/sub-projects/${project.system_code}?branch=${branch}&division=${divisionCode}&department=${departmentCode}&divisionName=${divisionName}&departmentName=${departmentName}&projectName=${project.system_desp_e}`
-              )
-            "
-            min-height="150"
-          >
+          <v-card elevation="1" color="whiteColor" outlined min-height="150">
             <v-list-item three-line>
               <v-list-item-content>
                 <v-list-item-subtitle>{{
-                  project.system_desp_e
+                  subProject.system_desp_e
                 }}</v-list-item-subtitle>
 
                 <v-list-item-subtitle>{{
-                  project.system_desp_a
+                  subProject.system_desp_a
                 }}</v-list-item-subtitle>
 
                 <v-list-item-subtitle
-                  >system_code: {{ project.system_code }}</v-list-item-subtitle
+                  >system_code:
+                  {{ subProject.system_code }}</v-list-item-subtitle
                 >
 
                 <v-list-item-subtitle
-                  >major_code: {{ project.major_code }}</v-list-item-subtitle
+                  >major_code: {{ subProject.major_code }}</v-list-item-subtitle
                 >
 
                 <v-list-item-subtitle
                   >section_code:
-                  {{ project.section_code }}</v-list-item-subtitle
+                  {{ subProject.section_code }}</v-list-item-subtitle
+                >
+
+                <v-list-item-subtitle
+                  >division_code:
+                  {{ subProject.division_code }}</v-list-item-subtitle
                 >
               </v-list-item-content>
             </v-list-item>
@@ -144,7 +143,7 @@
       <v-row v-else>
         <v-col>
           <h5 class="text-h6 pa-5">
-            {{ $t('adminPage.dtrApp.setup.noProjects') }}
+            {{ $t('adminPage.dtrApp.setup.noSubProjects') }}
           </h5>
         </v-col>
       </v-row>
@@ -156,42 +155,46 @@
 export default {
   layout: 'adminPage',
   asyncData({ params }) {
-    const departmentCode = params.projects
-    return { departmentCode }
+    const projectCode = params.subProjects
+    return { projectCode }
   },
   data() {
     return {
       overlay: false,
-      allProjects: [],
+      allSubProjects: [],
       branch: undefined,
       divisionCode: undefined,
+      departmentCode: undefined,
       divisionName: undefined,
       departmentName: undefined,
+      projectName: undefined,
     }
   },
   created() {
     if (this.$nuxt.context.query) {
       this.branch = this.$nuxt.context.query.branch
       this.divisionCode = this.$nuxt.context.query.division
+      this.departmentCode = this.$nuxt.context.query.department
       this.divisionName = this.$nuxt.context.query.divisionName
       this.departmentName = this.$nuxt.context.query.departmentName
+      this.projectName = this.$nuxt.context.query.projectName
     }
-    this.getProjectsPerDepartment()
+    this.getSubProjectsPerProject()
   },
-
   methods: {
-    async getProjectsPerDepartment() {
+    async getSubProjectsPerProject() {
       this.overlay = true
       try {
-        const projects = await this.$axios.post(
+        const subProjects = await this.$axios.post(
           `${this.$config.baseURL}/administration-api/hr-sql-call`,
           {
-            query: `SELECT system_desp_a, system_desp_e, section_code, major_code, system_code FROM [dbo].[pay_code_tables] WHERE branch_code='${this.branch}' and system_code_type='71'  
-                    and major_code='${this.divisionCode}' and section_code='${this.departmentCode}'`,
+            query: `SELECT system_desp_a, system_desp_e, system_code, major_code, section_code, division_code
+                    FROM [dbo].[pay_code_tables] WHERE branch_code='${this.branch}' and system_code_type='72'  
+                    and major_code='${this.divisionCode}' and section_code='${this.departmentCode}' and division_code ='${this.projectCode}'`,
           }
         )
-        if (projects.status === 200) {
-          this.allProjects = projects.data
+        if (subProjects.status === 200) {
+          this.allSubProjects = subProjects.data
           this.overlay = false
         }
       } catch (e) {
